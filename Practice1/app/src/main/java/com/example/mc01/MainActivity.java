3127244227a51ce58ecf6f0635e7fb17d4a75641
Zayyad Butt <75773764+Zayyad-Butt@users.noreply.github.com>
2021-03-11 16:42:03 +0500

    Added functionality of display message on button click

--- Practice1/app/src/main/java/com/example/mc01/MainActivity.java
+++ Practice1/app/src/main/java/com/example/mc01/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.mc01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button myButton;
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myButton =  findViewById(R.id.button2);
+        text = findViewById(R.id.textView);
+
+    }
+    public void displayMessage(View view){
+        text.setText("You are logged in successfully");
     }
 }
