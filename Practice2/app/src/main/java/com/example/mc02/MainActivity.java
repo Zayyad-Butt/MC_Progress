ac26bb44f8548638f40ac8af3510dd8df6bfb407
Zayyad Butt <75773764+Zayyad-Butt@users.noreply.github.com>
2021-03-11 16:43:08 +0500

    Changing Activity from one to another

---
+++ Practice2/app/src/main/java/com/example/mc02/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mc02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void MoveActivity(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+
+}
--- Practice2/app/src/main/java/com/example/mc02/MainActivity2.java
+++ Practice2/app/src/main/java/com/example/mc02/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.mc02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    public void pucitLink(View view) {
+        Uri uri = Uri.parse("http://www.pucit.edu.pk");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void callFunction(View view) {
+        Uri uri = Uri.parse("tel:+923244782111");
+        Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(intent);
     }
 }
