e579098d0a9c39505f1e9a1d4ef4f91fab380fb8
Zayyad Butt <75773764+Zayyad-Butt@users.noreply.github.com>
2021-05-26 18:22:41 +0500

    Fragments are updated

--- FragmentPractice/app/src/main/java/com/example/fragmentpractice/MainActivity.java
+++ FragmentPractice/app/src/main/java/com/example/fragmentpractice/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.fragmentpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        linearLayout=findViewById(R.id.linearLayout);
+
+    }
+
+    public void FragmentOne(View view) {
+        FragmentOne fragment1 = new FragmentOne();
+        FragmentTransaction transaction =getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.linearLayout, fragment1);
+        transaction.commit();
     }
 }
